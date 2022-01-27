bc4ec88d661ec53c9d9d8d86d79caebf5ab9cdb9
minhazemon21 <minhazkhondokar21@gmail.com>
2022-01-27 18:15:50 +0600

    Done CRUD

--- employee_register/urls.py
+++ employee_register/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .import views
 urlpatterns = [
-    path('', views.employee_form),
-    path('list/', views.employee_list),
+    path('', views.employee_form, name='employee_insert'), #insert data
+    path('<int:id>/', views.employee_form, name='employee_update'), #edit information
+    path('delete/<int:id>/', views.employee_delete, name='employee_delete'), #delete 
+    path('list/', views.employee_list, name='employee_list'), #display record
     path('test/', views.test),
 
 ]
