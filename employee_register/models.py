213d770d6e36743cab6ab6c7a4b44da124e569d7
Minhaz Uddin Emon <45569539+minhazemon21@users.noreply.github.com>
2022-05-22 17:16:53 +0600

    color

--- employee_register/models.py
+++ employee_register/models.py
@@ -11,5 +11,5 @@ class Position(models.Model):
 class Employee(models.Model):
     fullname = models.CharField(max_length=100)
     emp_code = models.CharField(max_length=3)
-    mobile = models.CharField(max_length=15)
+    mobile = models.CharField(max_length=11)
     position = models.ForeignKey(Position,on_delete=models.CASCADE)
